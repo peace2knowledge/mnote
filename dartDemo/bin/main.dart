4808718bb07d22e85a32ffd98a3b6eead4b2f67e
peace2knowledge <peace2knowledge@gmail.com>
2018-09-29 10:19:43 +0800

    fix

---
+++ dartDemo/bin/main.dart
@@ -0,0 +1,8 @@
+import 'package:dartDemo/dartDemo.dart' as dartDemo;
+import 'dart:async';
+main(List<String> arguments) {
+  Timer timer = new Timer(new Duration(seconds: 1), () {
+    print("Print after 1 seconds");
+  });
+ // print('Hello world: ${dartDemo.calculate()}!');
+}
